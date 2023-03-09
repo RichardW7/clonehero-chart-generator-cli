6aed7178afcd8266ea3c5630af9bac9875435174
m2wills <m2wills@uwaterloo.ca>
2023-03-09 14:36:59 -0500

    fixed note generation

--- src/createchartfile.py
+++ src/createchartfile.py
@@ -1,4 +1,8 @@
 def chart(track, sourceFile):
+    bpm = track.tempo
+    tpb = (track.time_signature / 4) * 96
+    tpm = tpb * track.time_signature
+
     # [Song]
     print("[Song]", file = sourceFile)
     print("{", file = sourceFile)
@@ -23,9 +27,19 @@ def chart(track, sourceFile):
     print("  0 = TS " + str(track.time_signature), file = sourceFile)
     print("  0 = B " + str(int(track.tempo * 1000)), file = sourceFile)
     
-    for i in track.sections:
-        print("  " + str(int(i * 1000)) + " = TS " + str(track.sections[i][1]), file = sourceFile)
-        print("  " + str(int(i * 1000)) + " = B " + str(int(track.sections[i][0] * 1000)), file = sourceFile)
+    # for i in track.sections:
+    #     total_beats = (i * 1000 * bpm) / 60
+    #     total_measures = total_beats / track.time_signature
+    #     ticks = int(total_measures * tpm)
+
+    #     print("  " + str(ticks) + " = TS " + str(track.sections[i][1]), file = sourceFile)
+    #     print("  " + str(ticks) + " = B " + str(int(track.sections[i][0] * 1000)), file = sourceFile)
+
+        # print("  " + str(int(i * 1000)) + " = TS " + str(track.sections[i][1]), file = sourceFile)
+        # print("  " + str(int(i * 1000)) + " = B " + str(int(track.sections[i][0] * 1000)), file = sourceFile)
+
+        # print("  " + str(int(track.sections[i][0] * 1000)) + " = TS " + str(track.sections[i][10]), file = sourceFile)
+        # print("  " + str(int(track.sections[i][0] * 1000)) + " = B " + str(int(track.sections[i][1] * 1000)), file = sourceFile)
 
     print("}", file = sourceFile)
 
@@ -39,23 +53,26 @@ def chart(track, sourceFile):
     print("{", file = sourceFile)
     in_use = {0: -1, 1: -1, 2: -1, 3: -1, 4: -1}
     for time_stamp_s in sorted(track.frets.keys()):
+        total_beats = (time_stamp_s * 2 * bpm) / 60
+        total_measures = total_beats / track.time_signature
+        ticks = int(total_measures * tpm)
         time_stamp_ms = int(time_stamp_s * 1000)
         for note in track.frets[time_stamp_s]:
             if time_stamp_ms >= in_use[0] and note[0] % 5 == 0:
                 in_use[0] = time_stamp_ms + note[1]
-                print("  " + str(time_stamp_ms) + " = N " + str(0) + " " + str(note[1]), file = sourceFile)
+                print("  " + str(ticks) + " = N " + str(0) + " " + str(int(note[1]/1000)), file = sourceFile)
             elif time_stamp_ms >= in_use[1] and note[0] % 5 == 1:
                 in_use[1] = time_stamp_ms + note[1]
-                print("  " + str(time_stamp_ms) + " = N " + str(1) + " " + str(note[1]), file = sourceFile)
+                print("  " + str(ticks) + " = N " + str(1) + " " + str(int(note[1]/1000)), file = sourceFile)
             elif time_stamp_ms >= in_use[2] and note[0] % 5 == 2:
                 in_use[2] = time_stamp_ms + note[1]
-                print("  " + str(time_stamp_ms) + " = N " + str(2) + " " + str(note[1]), file = sourceFile)
+                print("  " + str(ticks) + " = N " + str(2) + " " + str(int(note[1]/1000)), file = sourceFile)
             elif time_stamp_ms >= in_use[3] and note[0] % 5 == 3:
                 in_use[3] = time_stamp_ms + note[1]
-                print("  " + str(time_stamp_ms) + " = N " + str(3) + " " + str(note[1]), file = sourceFile)
+                print("  " + str(ticks) + " = N " + str(3) + " " + str(int(note[1]/1000)), file = sourceFile)
             elif time_stamp_ms >= in_use[4] and note[0] % 5 == 4:
                 in_use[4] = time_stamp_ms + note[1]
-                print("  " + str(time_stamp_ms) + " = N " + str(4) + " " + str(note[1]), file = sourceFile)
+                print("  " + str(ticks) + " = N " + str(4) + " " + str(int(note[1])), file = sourceFile)
             else:
                 continue
 
